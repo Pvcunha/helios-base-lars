022251a47bfcf70802da15fb73751c710e596c76
Pedro Cunha <pvc3@cin.ufpe.br>
2024-06-13 20:21:51 -0300

    learning models

---
+++ src/player/learning_models/learning_models.cpp
@@ -0,0 +1,36 @@
+#include "learning_models.h"
+
+LearningModels::OnnxModel::OnnxModel(const char *modelPath, Ort::Env *env, int numberOfThreads)
+{
+    // Set environment for our session
+    Ort::SessionOptions session_options;
+    session_options.SetIntraOpNumThreads(numberOfThreads); // number of threads should be a define, we may experience issues to change that in the future
+    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
+
+    // Load session
+    this->session = new Ort::Session(*env, modelPath, session_options);
+
+    // Get the properties of our model
+    this->numInputNodes = session->GetInputCount();
+    this->inputNodeNames = std::vector<char *>(this->numInputNodes);
+
+    for (int i = 0; i < numInputNodes; i++)
+    {
+        char *input_name = session->GetInputName(i, allocator);
+        this->inputNodeNames[i] = input_name;
+        Ort::TypeInfo typeInfo = session->GetInputTypeInfo(i);
+        auto tensorInfo = typeInfo.GetTensorTypeAndShapeInfo();
+        ONNXTensorElementDataType type = tensorInfo.GetElementType();
+        this->inputNodeDims = tensorInfo.GetShape();
+    }
+}
+
+//! ONNXModel destructor.
+/*!
+    This destructor must destroy every dynamic created array/object during any step of the class life cycle.
+*/
+LearningModels::OnnxModel::~OnnxModel()
+{
+    for (size_t i = 0; i < this->numInputNodes; i++)
+        allocator.Free(this->inputNodeNames[i]);
+}
